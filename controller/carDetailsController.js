189c7c50275a4b6dff7001d9c14e0fcbd6bce06e
Devdeep0109 <devdeepgoyal@gmail.com>
2024-11-17 12:39:33 +0530

    controller

--- controller/carDetailsController.js
+++ controller/carDetailsController.js
@@ -1,4 +1,42 @@
-const Car = require("../model/CarDetails");
+
+const {Schema ,model} = require("mongoose");
+
+const carSchema = new Schema(
+    {
+        title:{
+            type: String,
+            required: true,
+        },
+        coverImage:{
+            type : String,
+        },
+        price: {
+            type:Number,
+            required:true,
+        },
+        car_type:{
+            type:String,
+        },
+        company:{
+            type:String,
+
+        },
+        address:{
+            type:String,
+            required:true
+        },
+        additionalInfo:{
+            type:String
+        },
+        createdBy:{
+            type:Schema.Types.ObjectId,
+            ref:"user",
+        }
+    },{timestamps:true}
+)
+
+const Car = model("car",carSchema);
+
 
 
 
